97bcdbca3d44ecadba6fec4ca3dbc595be5e2c7e
Hrishikesh <hrishikeshbaidya7@gmail.com>
2022-04-01 15:11:02 +0530

    day1 to day 4

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+     //data types
+  //String
+  //double
+  // bool
+  //num
+  //var
+  //const
+  //final
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Catelog App"),
+        ),
+        body:Center(
+          child: Container(
+            child:Text("Welcome to $days days of flutter")
+        )
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
